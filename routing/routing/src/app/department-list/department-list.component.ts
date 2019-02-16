a605f4ffad0c127ac696a98584fb90ef6ee50ef3
jaworskimateusz <jaworskimateuszm@gmail.com>
2019-02-16 13:01:45 +0100

    Roting with Parameters

--- routing/routing/src/app/department-list/department-list.component.ts
+++ routing/routing/src/app/department-list/department-list.component.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-department-list',
   template: `
     <p>
-      department-list works!
+      Department List
     </p>
+    <ul class="items">
+      <li (click)="onSelect(department)" *ngFor="let department of departments">
+        <span class="badge"> {{department.id}}</span> {{department.name}}
+      </li>
+    </ul>
+
   `,
   styles: []
 })
 export class DepartmentListComponent implements OnInit {
 
-  constructor() { }
+  departments = [
+    {"id": 1, "name": "Angular"},
+    {"id": 2, "name": "Java"},
+    {"id": 3, "name": "MongoDB"},
+    {"id": 4, "name": "JavaScript"},
+    {"id": 5, "name": "Bootstrap"}
+  ]
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onSelect(department) {
+    this.router.navigate(['/departments', department.id]);
   }
 
 }
